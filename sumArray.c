5391f6105fb91cd7d8f54f1ec4852b03c6992344
Ankit Gupta <29058050+luckyhero613@users.noreply.github.com>
2019-10-14 19:18:10 +0530

    Create sumArray.c

---
+++ sumArray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    // define variable 
+    int a;
+
+    //Get input for number of elements
+    printf("Number of data you want to add: ");
+    scanf("%d", &a);
+    //define an array of integer 
+    int numArray[a];
+    int i;
+    //Get input for variables in an array
+    for(i=0; i<a; i++){
+        scanf("%d", &numArray[i]);
+    }
+    
+    int sum=0;
+    //Logic to add the variables
+    for(i=0; i<a; i++)
+    sum += numArray[i];
+    
+    printf("The sum of the variables is: %d\n", sum);  //Print the sum values
+}
